0fee9d75514912307586f49508e6414a8dc7a25b
vic1157 <ariyo90@gmail.com>
2025-04-06 11:06:11 -0400

    chore(backend): update users db table with jti column
    Implemented JTIMatching strategy with User model and users table

--- ezcater_clone_backend/db/schema.rb
+++ ezcater_clone_backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_05_004445) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_05_150929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -78,7 +78,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_05_004445) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
